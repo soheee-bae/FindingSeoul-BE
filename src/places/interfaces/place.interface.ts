79a0b57b607495ab278cff11724e60325fe099a5
SoHee <amybae28@gmail.com>
2024-12-14 17:51:52 +0900

    update Place interface

--- src/places/interfaces/place.interface.ts
+++ src/places/interfaces/place.interface.ts
@@ -1,3 +1,26 @@
 export interface Place {
   name: string;
+  id: number;
+  tel: string;
+  category: string[];
+  address: string;
+  roadAddress: string;
+  abbrAddress: string;
+  display: Node;
+  telDisplay: string;
+  thumUrl: string;
+  homePage: string;
+  description: string;
+  bizhourInfo: string;
+  menuInfo: string;
+  distance: string;
+  naverBookingUrl: string;
+  x: string;
+  y: string;
+  streetPanorama: {
+    id: string;
+    lng: string;
+    lat: string;
+    fov: string;
+  };
 }
