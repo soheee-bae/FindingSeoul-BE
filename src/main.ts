4fd09db0dd1190ba464749d002d541fdfb122f8d
SoHee <amybae28@gmail.com>
2025-01-01 17:11:05 +0900

    fixes on cors error

--- src/main.ts
+++ src/main.ts
@@ -6,6 +6,7 @@ import { NestExpressApplication } from '@nestjs/platform-express';
 async function bootstrap() {
   const app = await NestFactory.create<NestExpressApplication>(AppModule);
 
+  app.enableCors();
   // // Swagger
   // const config = new DocumentBuilder()
   //   .setTitle('Cats example')
