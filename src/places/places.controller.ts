850fd290b53228bd9a8deecdced6df18c598d49b
sohee28 <amybae28@gmail.com>
2025-04-08 22:43:38 +0900

    test

--- src/places/places.controller.ts
+++ src/places/places.controller.ts
@@ -13,10 +13,11 @@ export class PlacesController {
     @Query('search') search: string,
     @Query('siteSort') siteSort: number = 0, // relevant = 0, distance = 1
   ) {
-    const stationLabel = `${station}역`;
     // const apiUrl = search
     //   ? `https://m.map.naver.com/search2/searchMore.naver?query=${encodeURI(stationLabel)}${search ? `, ${search}` : ''}&siteSort=${siteSort}&sm=shistory&style=v5`
     //   : `https://m.map.naver.com/search2/interestSpotMore.naver?type=${type}&siteSort=${siteSort}&sm=clk&page=1&displayCount=${displayCount}`;
+
+    const stationLabel = `${station}역`;
 
     const apiUrl = `https://m.map.naver.com/search2/searchMore.naver?query=${encodeURI(stationLabel)}${search ? `, ${search}` : ''}&siteSort=${siteSort}&sm=shistory&style=v5&page=1&displayCount=${displayCount}`;
     return await this.placesService.findPlacesByStation(apiUrl);
